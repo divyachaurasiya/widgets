204229c198d124749a02d92cd3b358e1e8897c01
Divya Chaurasiya <Divyalisa@gmail.com>
2020-07-16 13:00:26 +0200

    First commit for the widgets

---
+++ src/components/Search.js
@@ -0,0 +1,46 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+
+const Search = () => {
+
+    const [term, setTerm] = useState('');
+    const [result, setResult] = useState([]);
+
+    useEffect(()=> { 
+       const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action : 'query',
+                    list : 'search',
+                    origin : '*',
+                    format :'json',
+                    srsearch : term,
+                },
+            });
+            setResult(data.query.search);
+        };
+        if (term) {
+            search();
+        }
+        
+
+        },
+        [term]);
+
+    return (
+        <div className="ui segment">
+        <div className="ui form">
+            <div className="field">
+            <label>Please enter the search term</label>
+            <input 
+            value={term}
+            onChange={(e)=> setTerm(e.target.value)}
+            className="input" type="text" />
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Search;
